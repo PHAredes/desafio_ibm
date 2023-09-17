fc461100626a2d3da4941a1040d72e9e248a74ba
pharedes <pedro.aredes@hotmail.com>
2023-09-17 00:10:14 -0300

    chore: project initialization

---
+++ src/main/java/com/example/demo/model/OrganizarTime.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+// está no model temporariamente, não é o local apropriado
+// substitua seu arquivo por essa classe
+public class OrganizarTime {
+
+    public static Map<String, List<String>> organizarTimes(List<String> jogadores) {
+        Map<String, List<String>> times = new HashMap<>();
+
+        jogadores.stream()
+                .filter(jogador -> jogador.split(" ").length == 2)
+                .forEach(jogador -> {
+                    String sobrenome = jogador.split(" ")[1];
+                    String chaveTime = sobrenomeJaExiste(times, sobrenome) ? "Time " + (times.size() + 1) : sobrenome.substring(0, 1);
+                    times.computeIfAbsent(chaveTime, key -> new ArrayList<>()).add(jogador);
+                });
+
+        return times;
+    }
+
+    private static boolean sobrenomeJaExiste(Map<String, List<String>> times, String sobrenome) {
+        return times.values().stream()
+                .flatMap(List::stream)
+                .map(j -> j.split(" ")[1])
+                .anyMatch(s -> s.equals(sobrenome));
+    }
+}
